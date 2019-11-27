965d4c489489078e671076745cdb95a1ff8ea630
unknown <warnetfightbb@163.com>
2019-11-27 14:25:47 +0800

    init

---
+++ src/main/java/com/bbdemo/bbdemo/BbdemoApplication.java
@@ -0,0 +1,13 @@
+package com.bbdemo.bbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bbdemo/bbdemo/BbdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbdemo.bbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
